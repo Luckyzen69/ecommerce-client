bc84ec6b8ee5644a180e0398f3c72e1caede91f0
Lagzen Thakuri <thakurizen2@gmail.com>
2023-09-20 07:55:14 +0545

    header-2

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,45 @@ function App() {
         <span><BsFillTelephoneFill className="inline" /> (1234)6789</span>
         </div>
         <div >
+          <select name="lang" id="lang" className=" bg-purple-500 capitalize" >
+            <option value="eng">English</option>
+            <option value="nep">Nepali</option>
+            <option value="jap">japanese</option>
+            <option value="kor">Korean</option>
+          </select>
+          <select name="dollar" id="dollar" className=" bg-purple-500 capitalize" >
+            <option value="eng">USD</option>
+            <option value="nep">RS</option>
+            <option value="jap">YEN</option>
+            <option value="kor">IDK</option>
+          </select>
           <span>Login<AiOutlineUser  className="inline " /> </span>
           <span> wishlist<AiOutlineHeart className="inline"/></span>
           <span><AiOutlineShoppingCart  className="inline" /> cart</span>
         </div>
       </header>
-      <div className="nav sm:flex sm:justify-center text-purple-700	 capitalize items-center text-center ">
+      <div className="nav sm:flex justify-around text-purple-700	 capitalize  text-center  ">
+      
         <h1 className="text-purple-900 text-2xl font-bold">hekto</h1>
-        <a href="#">home</a>
+       <select name="home" id="home" className="capitalize">
+        <option value="home" className="hidden">home</option>
+        <option value="product">chair</option>
+        <option value="product">shofa</option>
+        <option value="product">table</option>
+        <option value="product">bed</option>
+       </select>
         <a href="#">product</a>
         <a href="#">blog</a>
         <a href="#">shop</a>
         <a href="#">contact</a>
-        <span className="relative ">
-          <input className="border-solid border-2 hover:border-dotted" type="text" />
-          <AiOutlineSearch  className="absolute end-0 top-0 bottom-0 items-center" />
+        
+        
+        <div >
+        <span className="relative inline m-0 p-0 " >
+          <input className="border-solid border-2  hover:border-dotted" type="search" />
+          <AiOutlineSearch  className="absolute end-0 top-0 bottom-0   items-center bg-pink-600	outline-none text-2xl	hover:bg-red-700" />
         </span>
+        </div>
       </div>
     </>
   )
